bf368d63685e6517835d3db1df61360530d6eb52
allandonega <allan.s.donega@gmail.com>
2023-06-07 21:20:43 -0300

    Teste de recuperacao de nome de metodo

--- src/main/java/br/com/allandonega/HelloWorld.java
+++ src/main/java/br/com/allandonega/HelloWorld.java
@@ -6,19 +6,19 @@ import java.util.logging.Logger;
 
 public class HelloWorld {
     private static final Logger LOGGER = Logger.getLogger(HelloWorld.class.getName());
+    static Utils utils = new Utils();
 
     public HelloWorld() {
     }
-
-    static Utils utils = new Utils();
 
     public static void main(String[] args) {
         escreveMensagem("Hello World");
     }
 
     public static void escreveMensagem(String msg) {
-        System.out.println();
-        utils.log(msg);
+        utils.log("Metodo: [ " + Thread.currentThread().getStackTrace()[0].getMethodName() + " ] "+ msg);
+        utils.log("Metodo: [ " + Thread.currentThread().getStackTrace()[1].getMethodName() + " ] "+ msg);
+        utils.log("Metodo: [ " + Thread.currentThread().getStackTrace()[2].getMethodName() + " ] "+ msg);
     }
 
 }
